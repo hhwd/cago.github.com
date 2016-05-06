405ebfab8aa1f728aa33bc1d830f0ea470888182
white <white@30.30.180.163>
2016-05-06 19:31:44 +0800

    fix

---
+++ cago/src/main/java/com/hhw/cago/App.java
@@ -0,0 +1,13 @@
+package com.hhw.cago;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
